2c9969d815ddc9edba0b687d236be30ad900e99e
Adam Schirmacher <decker.adam@gmail.com>
2020-03-09 15:03:23 -0600

    Add stuff for work computer.

--- git-user-swap.sh
+++ git-user-swap.sh
@@ -14,6 +14,21 @@ useGitSettings () {
         export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
         export GIT_SSH_COMMAND="ssh -i /d/Users/Adam/.ssh_gusto/id_rsa"
         echo "Using Gusto..."
+    # Or work computer...
+    elif [[ "${PWD##/Users/adam.schirmacher/workspace/Decker87}" != "$PWD" ]]; then
+        export GIT_AUTHOR_NAME="Adam Schirmacher"
+        export GIT_AUTHOR_EMAIL="decker.adam@gmail.com"
+        export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+        export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+        export GIT_SSH_COMMAND="ssh -i /Users/adam.schirmacher/.ssh_personal/id_rsa"
+        echo "Using Decker87..."
+    elif [[ "${PWD##/Users/adam.schirmacher/workspace/Gusto}" != "$PWD" ]]; then
+        export GIT_AUTHOR_NAME="Adam Schirmacher"
+        export GIT_AUTHOR_EMAIL="adam.schirmacher@gusto.com"
+        export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+        export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+        export GIT_SSH_COMMAND="ssh -i /Users/adam.schirmacher/.ssh_gusto/id_rsa"
+        echo "Using Gusto..."
     else
         unset GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL GIT_COMMITTER_NAME GIT_COMMITTER_EMAIL SSH_OVERRIDE SSH_OVERRIDE_BEHAVIOR
         echo "Using NONE..."
